a96cb1f914e37c5384eae3c5e708cc3ca31861d7
Dipu9094 <dipu15-9094@diu.edu.bd>
2021-06-26 08:24:59 +0600

    Multiplication table using html and Js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let mainLi = document.getElementById('main-list')
 
 
 
-click.addEventListener('click', (e) => {
+click.addEventListener('click', (event) => {
 
     if (inputValue.value % 1 == 0) {
         while (mainLi.hasChildNodes()) {
@@ -15,8 +15,7 @@ click.addEventListener('click', (e) => {
 
             let list = document.createElement('li')
             list.style.backgroundColor = 'grey'
-            list.appendChild(document.createTextNode(`${inputValue.value} X ${i}= ${inputValue.value * i}`));
-
+            list.innerHTML = `${inputValue.value} X ${i}= ${inputValue.value * i}`
             mainLi.appendChild(list)
         }
         inputValue.value = ''
